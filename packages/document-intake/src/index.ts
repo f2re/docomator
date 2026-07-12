9b74014546d2c56561c5978c29ecce3426a82e14
F2re <f2re@users.noreply.github.com>
2026-07-12 14:05:19 +0300

    export secure document intake API

---
+++ packages/document-intake/src/index.ts
@@ -0,0 +1,17 @@
+export {
+  DEFAULT_INTAKE_LIMITS,
+  DocumentIntakeError,
+  inspectOoxmlBuffer
+} from "./intake.js";
+
+export type {
+  DocumentFormat,
+  DocumentIntakeReport,
+  InspectOoxmlInput,
+  IntakeDecision,
+  IntakeIssue,
+  IntakeIssueSeverity,
+  IntakeLimits,
+  IntakePart,
+  IntakeSummary
+} from "./intake.js";
