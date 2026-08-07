5dfdd54143527759da249410b15da38f8230c203
F2re <f2re@users.noreply.github.com>
2026-08-07 19:40:03 +0300

    refactor: move raw registries behind internal entrypoint

--- packages/storage/src/index-internal.ts
+++ packages/storage/src/index-internal.ts
@@ -1,10 +1,27 @@
 export {
   generateOpaqueStableKey,
   KnowledgeConflictError,
-  KnowledgeRegistry
+  KnowledgeNotFoundError,
+  KnowledgeRegistry,
+  KnowledgeValidationError
 } from "./knowledge.js";
 export type {
+  AppendPropertyValueInput,
+  CreateEntityInput,
+  CreateEntityTypeInput,
+  CreatePropertyDefinitionInput,
+  EntityRecord,
+  EntityStatus,
+  EntityTypeRecord,
+  ListEntitiesOptions,
+  ListPropertyValueHistoryOptions,
   MutationContext,
-  PropertyDefinitionRecord
+  PropertyCardinality,
+  PropertyDefinitionRecord,
+  PropertySensitivity,
+  PropertyUiGroup,
+  PropertyValueRecord
 } from "./knowledge.js";
+export { EmployeeRegistry } from "./employees.js";
+export { PublicationRegistry } from "./publications.js";
 export { stringifyJson, toJsonValue } from "./json.js";
